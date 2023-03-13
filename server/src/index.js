c7010f92d74d5d3f4ae2b4aba995e9372241ddd2
Borys Mch <borys.mch@gmail.com>
2023-03-13 12:24:13 +0200

    Added user routes with register and login in backend side

--- server/src/index.js
+++ server/src/index.js
@@ -2,14 +2,17 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
-dotenv.config();
+
+import { userRouter } from "./routes/users.js";
 
 const app = express();
-
+dotenv.config();
 const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter);
 
 mongoose.connect(MONGODB_URI);
 
