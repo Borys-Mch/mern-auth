530b70645afac2c5c41185c927182064cf903f3b
Borys Mch <borys.mch@gmail.com>
2023-03-13 15:43:53 +0200

    Added routes client and simple structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import CreateRecipe from "./pages/CreateRecipe";
+import SavedRecipes from "./pages/SavedRecipes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/create-recipe" element={<CreateRecipe />} />
+          <Route path="/saved-recipes" element={<SavedRecipes />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
