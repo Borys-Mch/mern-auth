78c6cf276749cd673401804a3e9ebb84a0822e5b
Borys Mch <borys.mch@gmail.com>
2023-03-21 17:47:58 +0200

    Created structure CreateRecipe page

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CreateRecipe = () => {
-  return <div>CreateRecipe</div>;
+  const [recipe, setRecipe] = useState({
+    name: "",
+    ingredients: [],
+    instructions: "",
+    imageUrl: "",
+    cookingTime: 0,
+    userOwner: 0,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setRecipe({ ...recipe, [name]: value });
+  };
+  return (
+    <div>
+      <h2>Create Recipe</h2>
+      <form>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" onChange={handleChange} />
+        <label htmlFor="description">Description</label>
+        <textarea
+          id="description"
+          name="description"
+          onChange={handleChange}></textarea>
+        <label htmlFor="ingredients">Ingredients</label>
+
+        <label htmlFor="instructions">Instructions</label>
+        <textarea
+          id="instructions"
+          name="instructions"
+          onChange={handleChange}></textarea>
+        <label htmlFor="imageUrl">Image URL</label>
+        <input
+          type="text"
+          id="imageUrl"
+          name="imageUrl"
+          onChange={handleChange}
+        />
+        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <input
+          type="number"
+          id="cookingTime"
+          name="cookingTime"
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default CreateRecipe;
