ff6da44aca028ad6c005a440bed3f05412aca6fe
Borys Mch <borys.mch@gmail.com>
2023-03-22 12:39:19 +0200

    Added css to CreateRecipe page

--- client/src/pages/CreateRecipe.jsx
+++ client/src/pages/CreateRecipe.jsx
@@ -15,36 +15,60 @@ const CreateRecipe = () => {
     setRecipe({ ...recipe, [name]: value });
   };
   return (
-    <div>
-      <h2>Create Recipe</h2>
-      <form>
-        <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" onChange={handleChange} />
-        <label htmlFor="description">Description</label>
+    <div className="container mx-auto flex flex-col">
+      <h2 className="mt-5 text-center text-lg font-bold">Create Recipe</h2>
+      <form className="flex flex-col mt-5 mx-auto w-full max-w-md">
+        <label htmlFor="name" className="text-lg">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleChange}
+          className=" p-2 border border-gray-300 rounded-lg"
+        />
+        <label htmlFor="description" className="text-lg">
+          Description
+        </label>
         <textarea
           id="description"
           name="description"
-          onChange={handleChange}></textarea>
-        <label htmlFor="ingredients">Ingredients</label>
+          onChange={handleChange}
+          className=" p-2 border border-gray-300 rounded-lg"
+        />
+        <label htmlFor="ingredients" className="text-lg">
+          Ingredients
+        </label>
 
-        <label htmlFor="instructions">Instructions</label>
+        <label htmlFor="instructions" className="text-lg">
+          Instructions
+        </label>
         <textarea
           id="instructions"
           name="instructions"
-          onChange={handleChange}></textarea>
-        <label htmlFor="imageUrl">Image URL</label>
+          onChange={handleChange}
+          className=" p-2 border border-gray-300 rounded-lg"
+        />
+        <label htmlFor="imageUrl" className="text-lg">
+          Image URL
+        </label>
         <input
           type="text"
           id="imageUrl"
           name="imageUrl"
           onChange={handleChange}
+          className=" p-2 border border-gray-300 rounded-lg"
         />
-        <label htmlFor="cookingTime">Cooking Time (minutes)</label>
+        <label htmlFor="cookingTime" className="text-lg">
+          Cooking Time (minutes)
+        </label>
         <input
           type="number"
           id="cookingTime"
           name="cookingTime"
           onChange={handleChange}
+          className=" p-2 border border-gray-300 rounded-lg"
         />
       </form>
     </div>
